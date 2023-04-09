14757a33080b047cbd1eae47767fce4e24098ab5
Phil <pk.oaferina@gmail.com>
2023-04-09 17:46:05 +0400

    added background mouse movement animation

--- index.js
+++ index.js
@@ -2,6 +2,32 @@
 // service_d91g7s3
 // Pjp-6drs8FHwQMi17
 
+let isModalOpen = false;
+let contrastToggle = false
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+	const shapes = document.querySelectorAll(".shape")
+	const x = event.clientX * scaleFactor;
+	const y = event.clientY * scaleFactor;
+
+	for (let i = 0; i < shapes.length; ++i) {
+		const isOdd = i % 2 !== 0;
+		const boolInt = isOdd ? -1 : 1;
+		shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+	}
+}
+
+function toggleContrast(){
+	contrastToggle = !contrastToggle
+	if(contrastToggle){
+		document.body.classList += "dark-theme"
+	}
+	else{
+		document.body.classList.remove("dark-theme")
+	}
+}
+
 function contact(event){
 	event.preventDefault()
 	const loading = document.querySelector('.modal__overlay--loading')
@@ -24,17 +50,13 @@ function contact(event){
 				"The email service is temporarily unavailable. Please contact me directly on pk.oaferina@gmail.com"
 			)
 		})
-
 }
 
-let isModalOpen = false;
-
-function toggleModal(){
-	if (isModalOpen){
-		isModalOpen = false;
-		return document.body.classList.remove("modal--open")
-	}
-
-	isModalOpen = !isModalOpen;
-	document.body.classList += " modal--open"
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
