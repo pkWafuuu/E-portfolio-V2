81f5029e037ff0bfa5554e57353d2cc077f79145
Phil <pk.oaferina@gmail.com>
2023-05-24 22:13:03 +0400

    feat: add animations

--- index.js
+++ index.js
@@ -3,53 +3,57 @@
 // Pjp-6drs8FHwQMi17
 
 let isModalOpen = false;
-let contrastToggle = false
+let contrastToggle = false;
 const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-	const shapes = document.querySelectorAll(".shape")
-	const x = event.clientX * scaleFactor;
-	const y = event.clientY * scaleFactor;
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
 
-	for (let i = 0; i < shapes.length; ++i) {
-		const isOdd = i % 2 !== 0;
-		const boolInt = isOdd ? -1 : 1;
-		shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
-	}
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform =
+      `translate(${x * boolInt}px, ${y * boolInt}px)` +
+      `rotate(${event.clientX * (1 / 3)}deg)` +
+      `rotate(${event.clientY * (1 / 3)}deg)`;
+  }
 }
 
-function toggleContrast(){
-	contrastToggle = !contrastToggle
-	if(contrastToggle){
-		document.body.classList += "dark-theme"
-	}
-	else{
-		document.body.classList.remove("dark-theme")
-	}
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += "dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
-function contact(event){
-	event.preventDefault()
-	const loading = document.querySelector('.modal__overlay--loading')
-	const success = document.querySelector('.modal__overlay--success')
-	
-	loading.classList += " modal__overlay--visible"
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
 
-	emailjs
-		.sendForm(
-			'service_d91g7s3',
-			'template_your7ex',
-			event.target,
-			'Pjp-6drs8FHwQMi17'
-		).then(() => {
-			loading.classList.remove("modal__overlay--visible")
-			success.classList += " modal__overlay--visible"
-		}).catch(() => {
-			loading.classList.remove("modal__overlay--visible")
-			alert(
-				"The email service is temporarily unavailable. Please contact me directly on pk.oaferina@gmail.com"
-			)
-		})
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_d91g7s3",
+      "template_your7ex",
+      event.target,
+      "Pjp-6drs8FHwQMi17"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on pk.oaferina@gmail.com"
+      );
+    });
 }
 
 function toggleModal() {
